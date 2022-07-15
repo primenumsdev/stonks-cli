fee4a6e0ff777249fc32ece08fbcf9186804495f
primenumsdev <code.primenums@gmail.com>
2022-07-15 18:12:52 +0200

    brew formula

---
+++ src/stonks/stonks-cli.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class StonksCli < Formula
+  desc "stonks-cli
+  homepage "https://github.com/primenumsdev/stonks-cli"
+  url "https://github.com/primenumsdev/stonks-cli/releases/download/v0.0.1/stonks-cli"
+  sha256 "dd25e75408224e7ab5fe0099c3d1d76f249a5754331e274899a1faff73465ada"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    #system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "stonks-cli"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test stonks-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/stonks-cli"
+  end
+end
